80d64aef51b4eab76b35e3704926e4af27631c3d
Remi Peyronnet <remi+github@via.ecp.fr>
2022-08-20 21:06:09 +0200

    Removed README copyand added note in bootstrap.sh and fix path for debian

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-# From https://github.com/rpeyron/plugin-gimp-fourier/pull/2
+# From https://github.com/rpeyron/plugin-gimp-fourier/pull/2 - DO NOT AUTOMATE
 
 wget -O config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
 wget -O config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
